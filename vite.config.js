d135782871a53986f029ad9d56552571457bc63c
Tom Snee <351700+tsnee@users.noreply.github.com>
2024-03-18 15:55:48 -0400

    First stab at publishing to GitHub Pages.

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,19 @@
 import { defineConfig } from "vite";
 import scalaJSPlugin from "@scala-js/vite-plugin-scalajs";
 
-export default defineConfig({
-  plugins: [scalaJSPlugin()],
+export default defineConfig(({ command }) => {
+  if (command === "serve") {
+    return {
+      server: {
+        open: true,
+      },
+      base: "/~tsnee/webgl/",
+      plugins: [scalaJSPlugin()],
+    }
+  } else {
+    return {
+      base: "https://tsnee.github.io/webgl-programming-guide-work/",
+      plugins: [scalaJSPlugin()],
+    }
+  }
 });
